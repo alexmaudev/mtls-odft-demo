fe0705e35a9010d64c57e5f1026bc4630fea6665
alekseim <aleksei.maurin@openet.com>
2023-01-16 16:44:30 +0000

    Fixed scripts

--- mtls-http-service-2/src/main/resources/add_to_the_truststore.sh
+++ mtls-http-service-2/src/main/resources/add_to_the_truststore.sh
@@ -1,4 +1,4 @@
 SERVICE_2_ALIAS=http-service-1
 PASSWORD=password
 EXEC_FOLDER=/home/$USER/mtls-demo/mtls-http-service-2/src/main/resources
-keytool -keystore $EXEC_FOLDER/server.truststore.jks -alias $SERVICE_2_ALIAS -import -file /home/aleksei/mtls-demo/mtls-http-service-1/src/main/resources/ca-cert -storepass $PASSWORD -keypass $PASSWORD -noprompt
+keytool -keystore $EXEC_FOLDER/server.truststore.jks -alias $SERVICE_2_ALIAS -import -file /home/$USER/mtls-demo/mtls-http-service-1/src/main/resources/ca-cert -storepass $PASSWORD -keypass $PASSWORD -noprompt
